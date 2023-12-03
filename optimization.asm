555fe95a3be15077cdec3867b4b60808aeeddcc8
Eugene <lolitsizigg@gmail.com>
2023-12-03 22:47:55 +0200

    111

---
+++ optimization.asm
@@ -0,0 +1,18 @@
+sum(int):
+        push    rbp
+        mov     rbp, rsp
+        mov     DWORD PTR [rbp-20], edi
+        mov     DWORD PTR [rbp-4], 0
+        mov     DWORD PTR [rbp-8], 0
+        jmp     .L2
+.L3:
+        mov     eax, DWORD PTR [rbp-8]
+        add     DWORD PTR [rbp-4], eax
+        add     DWORD PTR [rbp-8], 1
+.L2:
+        mov     eax, DWORD PTR [rbp-8]
+        cmp     eax, DWORD PTR [rbp-20]
+        jl      .L3
+        mov     eax, DWORD PTR [rbp-4]
+        pop     rbp
+        ret
